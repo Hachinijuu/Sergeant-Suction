a0f6f8bda695a321ac1e3944cac7810bb61e2a4c
cooper-al <157650773+cooper-al@users.noreply.github.com>
2024-03-31 14:36:01 -0400

    New Dialogue Manager
    - Added and set up dialogue manager script

---
+++ SergeantSuction_Project/Assets/Scripts/Managers/DialogeManager.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogeManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image lieutenantImage;
+    [SerializeField]
+    private Image sergeantImage;
+    [SerializeField]
+    private Text textbox;
+    [SerializeField]
+    private Image textbackground;
+    [SerializeField]
+    private List<string> dialogueLines;
+    [SerializeField]
+    private float typingTime;
+
+    private int currentLineIndex = 0;
+    private string currentLine;
+    private bool isTyping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!isTyping)
+            {
+                //currentLineIndex++;
+                if(currentLineIndex < dialogueLines.Count)
+                {
+                    ShowDialogue();
+                    SwapFace();
+                }
+                else
+                {
+                    isTyping = false;
+                    textbox.gameObject.SetActive(false);
+                    sergeantImage.gameObject.SetActive(false);
+                    lieutenantImage.gameObject.SetActive(false);
+                    textbackground.gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                StopAllCoroutines();
+                isTyping = false;
+                textbox.text = currentLine;
+            }
+        }
+    }
+
+    private void ShowDialogue()
+    {
+        currentLine = dialogueLines[currentLineIndex];
+        StartCoroutine(TypeText(currentLine));
+        currentLineIndex++;
+    }
+
+    IEnumerator TypeText(string text)
+    {
+        isTyping = true;
+        textbox.text = "";
+        foreach (char letter in text)
+        {
+            textbox.text += letter;
+            yield return new WaitForSeconds(typingTime);
+        }
+        isTyping = false;
+    }
+
+    private void SwapFace()
+    {
+        if(lieutenantImage.gameObject.activeSelf)
+        {
+            lieutenantImage.gameObject.SetActive(false);
+            sergeantImage.gameObject.SetActive(true);
+        }
+        else
+        {
+            lieutenantImage.gameObject.SetActive(true);
+            sergeantImage.gameObject.SetActive(false);
+        }
+    }
+}
